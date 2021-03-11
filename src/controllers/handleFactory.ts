5c633f2392ff9beb4e46df29ab674986872f5329
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2021-03-11 08:23:36 -0800

    add deleteOne and updateOne for factory functions

--- src/controllers/handleFactory.ts
+++ src/controllers/handleFactory.ts
@@ -56,3 +56,37 @@ export const getAll = (Model: Model<any>) => {
     });
   });
 };
+
+export const deleteOne = (Model: Model<any>) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError("No document found with that ID", 404));
+    }
+
+    res.status(204).json({ status: "success", data: null });
+  });
+};
+
+export const updateOne = (Model: Model<any>) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(
+        new AppError("No document found with that ID. Update failed", 404)
+      );
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: doc,
+      },
+    });
+  });
+};
