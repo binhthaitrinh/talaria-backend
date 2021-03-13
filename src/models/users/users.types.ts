1e934555ea38c333cbab1cb4867e9e7f5c6189df
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2021-03-13 15:56:23 -0800

    update types for user

--- src/models/users/users.types.ts
+++ src/models/users/users.types.ts
@@ -1,5 +1,38 @@
 import { Document, Model } from "mongoose";
 
+interface SocialMedia {
+  website: string;
+  link: string;
+}
+
+interface BankAcct {
+  bankName: string;
+  acctNumber: string;
+  bankLocation?: string;
+}
+
+interface CommissionRate {
+  website: string;
+  rate: number;
+}
+
+interface Address {
+  streetAddr: string;
+  city: string;
+}
+
+interface Profile {
+  updatedAt?: number;
+  socialMedias?: [SocialMedia];
+  phoneNumbers?: [string];
+  bankAccts?: [BankAcct];
+  commissionRates?: [CommissionRate];
+  dob?: number;
+  customerType?: string;
+  address?: [Address];
+  discountRates?: [CommissionRate];
+}
+
 export interface IUser {
   firstName: string;
   lastName?: string;
@@ -13,7 +46,7 @@ export interface IUser {
   customId?: string;
   createdAt: number;
   notes?: string;
-  profile: string;
+  profile: Profile;
 }
 
 export interface IUserDocument extends IUser, Document {
