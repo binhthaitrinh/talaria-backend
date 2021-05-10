f81816f7872ab901c14e22f8faaa91601b0b88f2
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2021-05-10 11:03:25 -0700

    remove getSequence

--- src/controllers/handleFactory.ts
+++ src/controllers/handleFactory.ts
@@ -6,24 +6,26 @@ import APIFeatures from '../utils/APIFeatures';
 // import { getNextSequence } from '../utils/getNextSequence';
 
 export const createOne = (Model: Model<any>) => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    // const name = Model.collection.collectionName.slice(
-    //   0,
-    //   Model.collection.collectionName.length - 1
-    // );
-    // const nextId = await getNextSequence(name, next);
-    const doc = await Model.create({
-      ...req.body,
-      // customId: `${name}-${nextId}`,
-    });
+  return catchAsync(
+    async (req: Request, res: Response, _next: NextFunction) => {
+      // const name = Model.collection.collectionName.slice(
+      //   0,
+      //   Model.collection.collectionName.length - 1
+      // );
+      // const nextId = await getNextSequence(name, next);
+      const doc = await Model.create({
+        ...req.body,
+        // customId: `${name}-${nextId}`,
+      });
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        data: doc,
-      },
-    });
-  });
+      res.status(201).json({
+        status: 'success',
+        data: {
+          data: doc,
+        },
+      });
+    }
+  );
 };
 
 export const getOne = (Model: Model<any>, options?: QueryOptions) => {
